e10b9affb5ea4ca6fbcbdeec7fe27c1f2ca7b159
Melissa <you@example.com>
2023-09-18 19:14:10 +0200

    working on the easy puzzle's

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 // alert("js ok");
 
 let yellowLine = document.getElementById("yellowLine");
-console.log("yellowLine");
+// console.log("yellowLine");
+let yellow = document.getElementById("yellow");
 
 let test = document.getElementById("test");
-console.log("test");
+// console.log("test");
 
 let txtImg = document.getElementById("txtImg");
-console.log("txtImg");
+// console.log("txtImg");
 
 
 test.addEventListener("mousedown", addFilter);
 
 function addFilter(){
     console.log("addFilter");
-    txtImg.style.display = "flex";
+    txtImg.style.display = "inline-block";
     yellowLine.style.height = "15px";
+    yellow.innerHTML = "90% FINI";
+    yellow.style.fontSize = "0.8em";
+    yellow.style.paddingLeft = "10px";
 }
